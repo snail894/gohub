2e2eea5539aae2fe7d1fbbac0bd5165c44687c93
snail894 <xiqi894@gamil.com>
2022-10-18 22:51:35 +0800

    集成 gin Hello:World!

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	// 初始化 Gin 实例
+	r := gin.Default()
+
+	// 注册一个路由
+	r.GET("/", func(c *gin.Context) {
+
+		// 以 JSON 格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+
+	// 运行服务
+	r.Run()
 }
